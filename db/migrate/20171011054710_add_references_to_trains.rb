9b7806418e45ee00cc9dbc1b169e2022cb56d7a7
Dima Rogov <ds.rogv@gmail.com>
2017-10-19 11:57:59 +0300

    Fix typo in migration method

--- db/migrate/20171011054710_add_references_to_trains.rb
+++ db/migrate/20171011054710_add_references_to_trains.rb
@@ -1,5 +1,5 @@
 class AddReferencesToTrains < ActiveRecord::Migration[5.1]
-  def cjange
+  def change
     change_table :trains do |t|
       t.references :route, foreign_key: true
       t.references :current_station, foreign_key: { to_table: :railway_stations }

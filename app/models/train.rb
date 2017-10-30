8ee97ad2682691f612b69be6076c4050abd56cf0
Dima Rogov <ds.rogv@gmail.com>
2017-10-30 16:28:42 +0300

    Add ability to count places in wagon

--- app/models/train.rb
+++ app/models/train.rb
@@ -8,4 +8,9 @@ class Train < ApplicationRecord
   def sorted_wagons
     wagons.sorted(head_direction)
   end
+
+  def count_places(wagon_type, place_type)
+    wagon_type = wagon_type.to_s.capitalize
+    wagons.where(wagon_type: wagon_type).sum("#{place_type}_places")
+  end
 end

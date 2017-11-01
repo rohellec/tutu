089733d603b5de52141705a24cc991eab057cdb0
Dima Rogov <ds.rogv@gmail.com>
2017-11-01 22:54:26 +0300

    Change scope syntax for railway_stations

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,7 +1,7 @@
 class RailwayStation < ApplicationRecord
-  scope(:route_order, lambda do
+  scope :route_order, -> {
     joins(:railway_stations_routes).order('railway_stations_routes.station_order')
-  end)
+  }
 
   has_many :trains, foreign_key: :current_station_id
   has_many :railway_stations_routes

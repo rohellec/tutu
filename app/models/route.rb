8f54b947e3beddec53a26ffb7b7352fc65c69997
Dima Rogov <ds.rogv@gmail.com>
2017-11-28 23:02:14 +0300

    Change search scope for route tickets

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,10 @@
 class Route < ApplicationRecord
-  scope :with_stations, ->(*stations) {
-    joins(:railway_stations_routes).where("railway_stations_routes.railway_station_id" => stations)
-                                   .uniq
+  scope :passed_through, ->(base, final) {
+    joins("INNER JOIN railway_stations_routes AS base ON routes.id = base.route_id")
+      .joins("INNER JOIN railway_stations_routes AS final ON routes.id = final.route_id")
+      .where("base.railway_station_id = :base AND final.railway_station_id = :final " \
+             "AND base.position < final.position", base: base, final: final)
+      .uniq
   }
 
   has_many :trains
@@ -10,21 +13,7 @@ class Route < ApplicationRecord
 
   validates :title, presence: true
 
-  def self.passed_through(base, final)
-    with_stations(base, final).find_all { |route| route.pass_through?(base, final) }
-  end
-
   def add_station(params)
     railway_stations_routes.create(params)
-  end
-
-  def pass_through?(base, final)
-    iterator = railway_stations.route_ordered.to_enum
-    loop do
-      iterator.next while iterator.peek != base
-      iterator.next while iterator.peek != final
-      return true
-    end
-    false
   end
 end

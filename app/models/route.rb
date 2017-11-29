259bb03212edacb2eab39b4752f7dcd602982bf5
Dima Rogov <ds.rogv@gmail.com>
2017-11-29 23:37:39 +0300

    Return pass_through? method to route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -16,4 +16,14 @@ class Route < ApplicationRecord
   def add_station(params)
     railway_stations_routes.create(params)
   end
+
+  def pass_through?(base, final)
+    iterator = railway_stations.route_ordered.to_enum
+    loop do
+      iterator.next while iterator.peek != base
+      iterator.next while iterator.peek != final
+      return true
+    end
+    false
+  end
 end
